b7bf0fe2bfb2d34f7f297d84ddc27454ade83422
TheXires <xires.dev@gmail.com>
2022-02-11 19:19:40 +0100

    fixed React has detected a change in the order of Hooks bug

--- src/components/NavigationHeaderButton.tsx
+++ src/components/NavigationHeaderButton.tsx
@@ -1,6 +1,6 @@
-import { useTheme } from '@react-navigation/native';
 import React from 'react';
 import { Pressable, StyleSheet, Text } from 'react-native';
+import { permanentColors } from '../theme/colors';
 
 interface Props {
   text: string;
@@ -8,11 +8,9 @@ interface Props {
 }
 
 function NavigationHeaderButton({ text, onPress }: Props) {
-  const { colors } = useTheme();
-
   return (
     <Pressable style={styles.container} onPress={onPress}>
-      <Text style={[styles.text, { color: colors.primary }]}>{text}</Text>
+      <Text style={[styles.text, { color: permanentColors.primary }]}>{text}</Text>
     </Pressable>
   );
 }
