45e27808f80ca86e5e563b1213db3c8daa09ffd7
TheXires <xires.dev@gmail.com>
2022-11-07 20:20:52 +0100

    moved i18n instance to i18n folder

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,6 +1,6 @@
 import { I18n } from 'i18n-js';
-import en from '../i18n/english';
-import de from '../i18n/german';
+import en from './english';
+import de from './german';
 
 // eslint-disable-next-line sort-keys
 export const i18n = new I18n({ en, 'de-DE': de });
