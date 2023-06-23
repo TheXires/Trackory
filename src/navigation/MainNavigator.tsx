186f23f7afd6b35ad633f0a2acea51ea4f45db56
TheXires <xires.dev@gmail.com>
2023-06-24 00:48:33 +0200

    Add basic setup for shared elements image

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -2,8 +2,8 @@
 // https://reactnavigation.org/docs/tab-based-navigation/
 // https://reactnavigation.org/docs/nesting-navigators/
 import { useNavigation } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 import NavigationHeaderButton from '../components/NavigationHeaderButton';
 import { i18n } from '../i18n/i18n';
 import AddItemScreen from '../screens/AddItemScreen';
@@ -14,7 +14,7 @@ import SettingsScreen from '../screens/SettingsScreen';
 import { RootStackParamList } from '../types/navigation';
 import BottomNavigator from './BottomNavigator';
 
-const RootStack = createStackNavigator<RootStackParamList>();
+const RootStack = createSharedElementStackNavigator<RootStackParamList>();
 
 function RootStackNavigator() {
   const navigation = useNavigation();
@@ -53,25 +53,29 @@ function RootStackNavigator() {
       <RootStack.Screen
         name="ItemDetails"
         component={ItemDetailsScreen}
+        sharedElements={(route) => {
+          const { itemId } = route.params;
+          return [`item.${itemId}.image`];
+        }}
         options={{
           headerBackTitle: i18n.t('back'),
           title: i18n.t('itemDetailsTitle'),
         }}
       />
-      <RootStack.Group screenOptions={{ presentation: 'modal' }}>
-        <RootStack.Screen
-          name="EditItem"
-          component={EditItemScreen}
-          options={{
-            headerLeft: () =>
-              NavigationHeaderButton({
-                onPress: () => navigation.goBack(),
-                text: i18n.t('cancel'),
-              }),
-            title: i18n.t('EditItemTitle'),
-          }}
-        />
-      </RootStack.Group>
+      {/* <RootStack.Group screenOptions={{ presentation: 'modal' }}> */}
+      <RootStack.Screen
+        name="EditItem"
+        component={EditItemScreen}
+        options={{
+          headerLeft: () =>
+            NavigationHeaderButton({
+              onPress: () => navigation.goBack(),
+              text: i18n.t('cancel'),
+            }),
+          title: i18n.t('EditItemTitle'),
+        }}
+      />
+      {/* </RootStack.Group> */}
     </RootStack.Navigator>
   );
 }
